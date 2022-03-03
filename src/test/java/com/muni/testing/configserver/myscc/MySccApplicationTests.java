ee2e797d01fff94a6c05421ab2e623cffe18b231
Mk <munikumar450@gmail.com>
2022-03-02 23:38:14 -0700

    my scc project

---
+++ src/test/java/com/muni/testing/configserver/myscc/MySccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muni.testing.configserver.myscc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
